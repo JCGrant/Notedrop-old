e288a1f73a9dfecbc28587d19212e2fb08459d16
JCGrant <jamescolin.grant@gmail.com>
2016-12-31 23:06:18 +0000

    Add Note SQLAlchemy model

---
+++ server/models.py
@@ -0,0 +1,18 @@
+from server import db
+
+class Note(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    text = db.Column(db.Text)
+    longitude = db.Column(db.Float)
+    latitude = db.Column(db.Float)
+
+    def __init__(self, text, longitude, latitude):
+        self.text = text
+        self.longitude = longitude
+        self.latitude = latitude
+
+    def __repr__(self):
+        return "<Note @ {}-{}> {}".format(
+            self.longitude,
+            self.latitude,
+            self.text)
