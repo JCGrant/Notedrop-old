c12f337385805b86bc2f9e3ec8851a7651149f55
JCGrant <jamescolin.grant@gmail.com>
2017-01-01 00:11:56 +0000

    Return nearby notes on GET request

--- server/views.py
+++ server/views.py
@@ -3,7 +3,15 @@ from .models import Note
 from flask import request
 import json
 
+NEARBY_DISTANCE = 0.001
+
 @app.route('/notes', methods=['GET'])
 def get_nearby_notes():
-    notes = Note.query.all()
+    longitude = float(request.args.get('longitude'))
+    latitude = float(request.args.get('latitude'))
+    all_notes = Note.query.all()
+    notes = [note for note in all_notes if
+        (note.longitude - longitude) ** 2 +
+        (note.latitude - latitude) ** 2 <=
+        NEARBY_DISTANCE ** 2]
     return json.dumps([note.jsonify() for note in notes])
