ce53a46e260cc21778a0f381fe88317e4991df9c
JCGrant <jamescolin.grant@gmail.com>
2017-01-06 20:37:55 +0000

    Add if __name__ == '__main__'

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 from server import app
 
-app.run(debug=True, host='0.0.0.0', ssl_context=('server.crt', 'server.key'))
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', ssl_context=('server.crt', 'server.key'))
