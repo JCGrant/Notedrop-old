beee862009b0a42790dd552cc7a45786717259a1
JCGrant <jamescolin.grant@gmail.com>
2017-01-07 13:10:08 +0000

    Replace SQLALCHEMY_DATABASE_URI with environment variable

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,11 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.routing import FloatConverter
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
